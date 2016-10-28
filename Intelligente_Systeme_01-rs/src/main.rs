0dc2a3be10991c62e9a53adbf03cd7fd28779180
Lukas Deichmann <lukasdeichmann+github@gmail.com>
2016-10-28 10:01:45 +0200

    Use 12 hours and evaluate all results

--- Intelligente_Systeme_01-rs/src/main.rs
+++ Intelligente_Systeme_01-rs/src/main.rs
@@ -5,13 +5,16 @@ use time::precise_time_ns;
 use std::io::prelude::*;
 use std::fs::File;
 use std::thread;
+use std::sync::{Arc, Mutex};
 
 const NUM_LOCKERS: usize = 150;
 const TIME_TO_CHANGE: usize = 30; // 5 * 6 = 30
-const RUNTIME: usize = 3600;
+const RUNTIME: usize = 4320;
 const CUSTOMER_PROBABILITY: u8 = 1;
 const CUSTOMER_PROBABILITY_MAX: u8 = 10;
-static NTHREADS: i32 = 10;
+const NTHREADS: i32 = 10;
+const FOCUS_BEGIN: i16 = 1770;
+const FOCUS_END: i16 = 1830;
 
 #[derive(Clone, Debug, Copy, PartialEq)]
 enum LockerState {
@@ -27,6 +30,8 @@ struct Locker {
     occupy_time: i16,
     state: LockerState,
     had_encounter: bool,
+    focus: bool,
+    focus_counted: bool
 }
 
 // impl of Val
@@ -45,10 +50,16 @@ impl Locker {
         self.had_encounter = false;
     }
 
+    pub fn reset_focus_counted(&mut self) {
+        self.focus_counted = false;
+    }
+
     pub fn free_locker(&mut self) {
         self.state = LockerState::Free;
         self.return_time = 0;
         self.reset_encounter();
+        self.reset_focus_counted();
+        self.focus = false;
 //        println!("Locker {} free", self.id)
     }
 
@@ -70,6 +81,7 @@ impl Locker {
             } else if time == self.occupy_time + (TIME_TO_CHANGE as i16) {
                 self.occupy_locker();
                 self.reset_encounter();
+                self.reset_focus_counted();
             }
         }
     }
@@ -90,7 +102,7 @@ fn update_lockers(lr: &mut [Locker], time: i16, occupied_lockers: &mut i16) {
 
 fn check_new_customer() -> bool {
     let mut rng = thread_rng();
-    let cst: u8 = rng.gen_range(1, CUSTOMER_PROBABILITY_MAX);
+    let cst: u8 = rng.gen_range(1, CUSTOMER_PROBABILITY_MAX+1);
     cst == CUSTOMER_PROBABILITY
 }
 
@@ -111,13 +123,15 @@ fn get_return_time(input_data: &Vec<i16>) -> i16 {
     input_data[nr]
 }
 
-fn new_customer(lr: &mut [Locker], time: i16, occupied_lockers: &mut i16, input_data: &Vec<i16>) {
+fn new_customer(lr: &mut [Locker], time: i16, occupied_lockers: &mut i16, input_data: &Vec<i16>) -> i16 {
     if *occupied_lockers == (NUM_LOCKERS as i16) {
-        println!("Error: All lockers occupied")
+        println!("Error: All lockers occupied");
+        -1
     } else {
         let locker_number = get_random_free_locker(lr);
         lr[(locker_number as usize)].assign_locker(time, time+get_return_time(input_data));
         *occupied_lockers = *occupied_lockers + 1;
+        locker_number
 //        println!("New customer in locker {}, free at {}, occupied lockers {}", locker_number, lr[(locker_number as usize)].return_time, occupied_lockers)
     }
 
@@ -169,15 +183,24 @@ fn detect_encounters(lr: &mut [Locker], encounters: &mut i16) {
             }
         }
     }
-
 }
 
-fn simulation(input_data: &Vec<i16>) {
-    let mut locker_array: [Locker; NUM_LOCKERS] = [Locker { id: 0, return_time: 0, occupy_time: 0, state: LockerState::Free, had_encounter: false}; NUM_LOCKERS];
+fn detect_focus_encounter(lr: &mut [Locker], id: &i16, encounters: &mut i16) {
+    let id = *id as usize;
+    if lr[id].had_encounter && !lr[id].focus_counted {
+        *encounters = *encounters + 1;
+        lr[id].focus_counted = true;
+    }
+}
+
+fn simulation(input_data: &Vec<i16>) -> (i16, i16, i16){
+    let mut locker_array: [Locker; NUM_LOCKERS] = [Locker { id: 0, return_time: 0, occupy_time: 0, state: LockerState::Free, had_encounter: false, focus: false, focus_counted: false}; NUM_LOCKERS];
     initialize_lockers(&mut locker_array);
     let mut occupied_lockers: i16 = 0;
     let mut customers: i16 = 0;
     let mut encounters: i16 = 0;
+    let mut focus_locker_id: i16 = -1;
+    let mut focus_encounters: i16 = 0;
 
     let mut i = 0;
     while i < (RUNTIME as i16) {
@@ -185,16 +208,29 @@ fn simulation(input_data: &Vec<i16>) {
         update_lockers(&mut locker_array, i, &mut occupied_lockers);
         if check_new_customer() {
             customers = customers + 1;
-            new_customer(&mut locker_array, i, &mut occupied_lockers, input_data)
+            let id: i16 = new_customer(&mut locker_array, i, &mut occupied_lockers, input_data);
+            if FOCUS_BEGIN <= i && i <= FOCUS_END {
+                focus_locker_id = id;
+                locker_array[focus_locker_id as usize].focus = true;
+            }
+        }
+        // if we were really unlucky and didn't get a focus person, force it
+        if i > FOCUS_END && focus_locker_id == -1 {
+            focus_locker_id = new_customer(&mut locker_array, i, &mut occupied_lockers, input_data);
+            locker_array[focus_locker_id as usize].focus = true;
         }
         detect_encounters(&mut locker_array, &mut encounters);
+        if focus_locker_id != -1 && locker_array[focus_locker_id as usize].focus {
+            detect_focus_encounter(&mut locker_array, &focus_locker_id, &mut focus_encounters)
+        }
         i = i + 1;
     }
 
 //    println!("Total customers: {}", customers);
 //    println!("Total encounters: {}", encounters);
-    print!("{}", format!("Total customers {}\n", customers));
-    print!("{}", format!("Total encounters {}\n", encounters));
+//    print!("{}", format!("Total customers {}\n", customers));
+//    print!("{}", format!("Total encounters {}\n", encounters));
+    (customers, encounters, focus_encounters)
 }
 
 
@@ -224,19 +260,21 @@ fn main() {
     }
 
     let mut children = vec![];
+    let results_vec: Vec<(i16, i16, i16)> = vec![];
+    let results_container = Arc::new(Mutex::new(results_vec));
     let tm3 = precise_time_ns();
-    //    while i < 10000 {
-    //        simulation(&input_data);
-    //        i = i + 1;
-    //    }
     for _ in 0..NTHREADS {
         let i_data = input_data.clone();
+        let total_data = results_container.clone();
         children.push(thread::spawn(move || {
             let mut j = 0;
+            let mut results: Vec<(i16, i16, i16)> = vec![];
             while j < 1000 {
-                simulation(&i_data.clone());
+                results.push(simulation(&i_data.clone()));
                 j = j + 1;
             }
+            let mut total_data = total_data.lock().unwrap();
+            total_data.append(&mut results);
         }));
     }
     for child in children {
@@ -245,4 +283,16 @@ fn main() {
     }
     let tm4 = precise_time_ns();
     println!("{}ns", tm4-tm3);
+    let results = results_container.clone();
+    let m = results.lock().unwrap();
+    let mut sum_cust = 0.0_f32;
+    let mut sum_enc = 0.0_f32;
+    let mut sum_foc_enc = 0.0_f32;
+    for a in m.iter() {
+//        println!("{} {} {}", a.0, a.1, a.2);
+        sum_cust = sum_cust + a.0 as f32;
+        sum_enc = sum_enc + a.1 as f32;
+        sum_foc_enc = sum_foc_enc + a.2 as f32;
+    }
+    println!("Per day:\n\tAverage customers:{}\n\tAverage encounters: {}\n\tAverage focus person encounters: {}", sum_cust/10000.0, sum_enc/10000.0, sum_foc_enc/10000.0)
 }
