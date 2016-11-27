1fb01f62ad6199de2a46d361c802a78de67b981b
Lukas Deichmann <lukasdeichmann+github@gmail.com>
2016-11-27 20:49:48 +0100

    Calculate recall, precision and f-score for labels

---
+++ Intelligente_Systeme_02-java/src/analysis/Score.java
@@ -0,0 +1,120 @@
+package analysis;
+
+import geometry.Point;
+import parsing.csv;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Score {
+
+    private double precision;
+    private double recall;
+    private double fscore;
+
+    private List<Point> points;
+    private List<Point> labels;
+    private List<Point> foundLabels;
+    private List<Point> labelCluster;
+
+    /**
+     * Creates a Score instance
+     * @param points List of all points
+     * @param labels List of labels within points
+     * @param foundLabels List of labels to be evaluated
+     */
+    public Score(List<Point> points, List<Point> labels, List<Point> foundLabels) {
+        this.points = points;
+        this.labels = labels;
+        this.foundLabels = foundLabels;
+    }
+
+    /**
+     * Finds acceptable points around a given label
+     * @param label the label from which to span the cluster
+     * @return  a list containing the points
+     */
+    private List<Point> spanClusterForLabel(Point label) {
+        List<Point> returnList = new ArrayList<>();
+        List<Point> neighbours = new ArrayList<>();
+
+        returnList.add(label);
+        for (int i = 1; i <= 5; i++) {
+            neighbours.addAll(label.getNearPoints(i));
+        }
+        for (Point x : neighbours){
+            int index = this.points.indexOf(x);
+            if (index >= 0 && !returnList.contains(x) && this.points.get(index).getZ() >= label.getZ())
+                returnList.add(x);
+        }
+        return returnList;
+    }
+
+    /**
+     * Finds all acceptable points for all labels
+     */
+    private void spanClusterForLabels() {
+        List<Point> returnList = new ArrayList<>();
+        for (int i = 0; i < this.labels.size(); i++) {
+            returnList.addAll(spanClusterForLabel(this.labels.get(i)));
+            System.out.println("Label " + (i+1) + "/" + this.labels.size());
+        }
+        this.labelCluster = returnList;
+    }
+
+    /**
+     * Calculates precision from label clusters and found labels
+     */
+    private void calculatePrecision() {
+        double correctLabels = 0.0;
+        for (Point p : this.foundLabels) {
+            if (this.labelCluster.contains(p)) {
+                correctLabels++;
+                continue;
+            }
+        }
+        this.precision = correctLabels / (double)this.foundLabels.size();
+    }
+
+    /**
+     * Calculates recall from label clusters and found labels
+     */
+    private void calculateRecall() {
+        double correctLabels = 0.0;
+        for (Point p : this.foundLabels) {
+            if (this.labelCluster.contains(p)) {
+                correctLabels++;
+                continue;
+            }
+        }
+        this.recall = correctLabels / (double)this.labels.size();
+    }
+
+    /**
+     * Calculates the F-Score for the given data
+      * @return f-score
+     */
+    public double calculateScore() {
+        this.spanClusterForLabels();
+        this.calculatePrecision();
+        this.calculateRecall();
+        this.fscore = (2.0 * this.recall * this.precision)/(this.recall + this.precision);
+        return this.fscore;
+    }
+
+//    public static void main(String[] args) throws FileNotFoundException {
+//        List<Point> pointList = csv.getPointsFromCSV(new File("/home/cru/Downloads/ISys_02/data0.csv"));
+//        List<Point> labelList = csv.getLabelsFromCSV(new File("/home/cru/Downloads/ISys_02/label0.csv"), pointList);
+//        List<Point> calcLabel = csv.getLabelsFromCSV(new File("/home/cru/Downloads/ISys_02/rommel.csv"), pointList);
+//
+//        Score s = new Score(pointList, labelList, calcLabel);
+//
+//        s.calculateScore();
+//        System.out.println("Recall: " + s.recall);
+//        System.out.println("Precision: " + s.precision);
+//        System.out.println("F-Score: " + s.fscore);
+//        csv.writePointstoCSV(new File("/home/cru/Downloads/ISys_02/clustertest.csv"), s.labelCluster, false);
+//    }
+}
