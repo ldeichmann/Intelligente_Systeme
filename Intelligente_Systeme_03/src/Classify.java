846b7ec35b316f3a159422083d4351ccb74a26a1
Lukas Deichmann <lukasdeichmann+github@gmail.com>
2017-01-12 14:25:22 +0100

    Add pretty printing of the matrix and results

--- Intelligente_Systeme_03/src/Classify.java
+++ Intelligente_Systeme_03/src/Classify.java
@@ -26,11 +26,8 @@ public class Classify {
     public static void classifyDifferenceVectors(List<DifferenceVector> vecs, double threshold) throws Exception {
 
         for (DifferenceVector v: vecs) {
-            String tmp = new String();
-
-            tmp = tmp.concat(classifyDifferenceVector(v.getDiff_x(), threshold));
-            tmp = tmp.concat(classifyDifferenceVector(v.getDiff_y(), threshold));
-
+            String tmp = classifyDifferenceVector(v.getDiff_x(), threshold)
+                       + classifyDifferenceVector(v.getDiff_y(), threshold);
             v.setState(State.valueOf(tmp));
         }
 
@@ -45,59 +42,44 @@ public class Classify {
             for (State s2: State.values()) {
                 String pair = s.name() + s2.name();
                 pairMap.put(pair, 1.0);
+                countMap.putIfAbsent(s, 0.0);
+                countMap.put(s, countMap.get(s)+1.0);
             }
-            countMap.put(s, 1.0);
         }
-//        System.out.println(pairMap);
-//        System.out.println(countMap);
-
         for (int i = 0; i < vecs.size()-1; i++) {
             DifferenceVector vec = vecs.get(i);
             DifferenceVector vec2 = vecs.get(i+1);
-            String pair = new String();
-            pair = pair.concat(vec.getState().name());
-            pair = pair.concat(vec2.getState().name());
-
-            countMap.putIfAbsent(vec.getState(), 0.0);
+            String pair = vec.getState().name() + vec2.getState().name();
             countMap.put(vec.getState(), countMap.get(vec.getState())+1);
-            pairMap.putIfAbsent(pair, 0.0);
             pairMap.put(pair, pairMap.get(pair)+1);
-            // This is so terrible, I can't stop laughing
-//            switch (vec.getState().name().charAt(0)) {
-//                case 'A':
-//                    a++;
-//                    break;
-//                case 'B':
-//                    b++;
-//                    break;
-//                case 'C':
-//                    c++;
-//                    break;
-//            }
         }
 
-        pairMap.forEach( (k,v) -> pairMap.put(k, v / countMap.get( State.valueOf(k.substring(2)) )) );
-
-//        for (State s : State.values()) {
-//            switch (s.name().charAt(0)) {
-//                case 'A':
-//                    countMap.put(s, countMap.get(s)/a);
-//                    break;
-//                case 'B':
-//                    countMap.put(s, countMap.get(s)/b);
-//                    break;
-//                case 'C':
-//                    countMap.put(s, countMap.get(s)/c);
-//                    break;
-//            }
-//        }
+        pairMap.forEach( (k,v) -> pairMap.put(k, v / countMap.get( State.valueOf(k.substring(0, 2)) )) );
 
         return pairMap;
     }
 
+    public static void printMatrix(Map<String, Double> matrix) {
+
+        System.out.print("     ");
+        for (State s: State.values()) {
+            System.out.print(s.name() + "     ");
+        }
+        System.out.println();
+        for (State s: State.values()) {
+            System.out.print(s.name() + " ");
+            for (State s2: State.values()) {
+                System.out.format("%.4f ", matrix.get(s.name()+s2.name()));
+            }
+            System.out.println();
+        }
+
+
+    }
+
     public static void main(String[] args) throws Exception {
 
-        double threshold = 10;
+        double threshold = 14;
 
         List<List<Vector>> vecs_alone = ReadData.getVectorsFromFile(new File("train_alone.txt"));
         List<List<Vector>> vecs_group = ReadData.getVectorsFromFile(new File("train_group.txt"));
@@ -121,12 +103,10 @@ public class Classify {
         Map<String, Double> counted_alone = countClassifications(diff_vecs_alone);
         Map<String, Double> counted_group = countClassifications(diff_vecs_group);
 
-        System.out.println(counted_alone);
+        List<List<Vector>> eval_vecs = ReadData.getVectorsFromFile(new File("eval_alone.txt"));
 
-        List<List<Vector>> eval_vecs = ReadData.getVectorsFromFile(new File("eval_group.txt"));
-
-        int count_alone = 0;
-        int count_group = 0;
+        double count_alone = 0;
+        double count_group = 0;
 
         for (List<Vector> eval_v : eval_vecs) {
 
@@ -140,7 +120,6 @@ public class Classify {
             double tmp_alone = 0;
             double tmp_group = 0;
 
-//            for (DifferenceVector eval_vec : eval_diff_vecs) {
             for (int i = 0; i < eval_diff_vecs.size()-1; i++) {
                 DifferenceVector eval_vec = eval_diff_vecs.get(i);
                 DifferenceVector eval_vec2 = eval_diff_vecs.get(i+1);
@@ -155,10 +134,12 @@ public class Classify {
                 count_alone++;
             }
 
-//            System.out.println(tmp_alone + " - " + tmp_group);
-
         }
+        printMatrix(counted_alone);
         System.out.println("Alone: " + count_alone + "\nGroup: " + count_group);
+        System.out.format("Percent alone: %.4f%%\n",count_alone/((count_alone+count_group)));
+        System.out.format("Percent group: %.4f%%\n", count_group/((count_alone+count_group)));
+
     }
 
 }
