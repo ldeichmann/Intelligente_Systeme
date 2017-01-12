2f3aefde696ad2edd2a3609531acc69bbdda4f77
Lukas Deichmann <lukasdeichmann+github@gmail.com>
2017-01-12 12:48:31 +0100

    9x9 matrix

--- Intelligente_Systeme_03/src/Classify.java
+++ Intelligente_Systeme_03/src/Classify.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 public class Classify {
 
-    public static String classifyDifferenceVector(double value, double threshold) {
+    public static String classifyDifferenceVector(double value, double threshold) throws Exception {
 
         if (value < (-1 * threshold)) {
             return "A";
@@ -20,10 +20,10 @@ public class Classify {
         } else if (threshold < value) {
             return "C";
         }
-        return "F";
+        throw new Exception("Cannot classify!");
     }
 
-    public static void classifyDifferenceVectors(List<DifferenceVector> vecs, double threshold) {
+    public static void classifyDifferenceVectors(List<DifferenceVector> vecs, double threshold) throws Exception {
 
         for (DifferenceVector v: vecs) {
             String tmp = new String();
@@ -36,50 +36,68 @@ public class Classify {
 
     }
 
-    public static Map<State, Double> countClassifications(List<DifferenceVector> vecs) {
+    public static Map<String, Double> countClassifications(List<DifferenceVector> vecs) {
+        Map<String, Double> pairMap = new HashMap<>();
         Map<State, Double> countMap = new HashMap<>();
 
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        // set everything to 1
+        for (State s: State.values()) {
+            for (State s2: State.values()) {
+                String pair = s.name() + s2.name();
+                pairMap.put(pair, 1.0);
+            }
+            countMap.put(s, 1.0);
+        }
+//        System.out.println(pairMap);
+//        System.out.println(countMap);
 
-        for (DifferenceVector vec : vecs) {
+        for (int i = 0; i < vecs.size()-1; i++) {
+            DifferenceVector vec = vecs.get(i);
+            DifferenceVector vec2 = vecs.get(i+1);
+            String pair = new String();
+            pair = pair.concat(vec.getState().name());
+            pair = pair.concat(vec2.getState().name());
+
             countMap.putIfAbsent(vec.getState(), 0.0);
             countMap.put(vec.getState(), countMap.get(vec.getState())+1);
+            pairMap.putIfAbsent(pair, 0.0);
+            pairMap.put(pair, pairMap.get(pair)+1);
             // This is so terrible, I can't stop laughing
-            switch (vec.getState().name().charAt(0)) {
-                case 'A':
-                    a++;
-                    break;
-                case 'B':
-                    b++;
-                    break;
-                case 'C':
-                    c++;
-                    break;
-            }
+//            switch (vec.getState().name().charAt(0)) {
+//                case 'A':
+//                    a++;
+//                    break;
+//                case 'B':
+//                    b++;
+//                    break;
+//                case 'C':
+//                    c++;
+//                    break;
+//            }
         }
 
-        for (State s : State.values()) {
-            switch (s.name().charAt(0)) {
-                case 'A':
-                    countMap.put(s, countMap.get(s)/a);
-                    break;
-                case 'B':
-                    countMap.put(s, countMap.get(s)/b);
-                    break;
-                case 'C':
-                    countMap.put(s, countMap.get(s)/c);
-                    break;
-            }
-        }
+        pairMap.forEach( (k,v) -> pairMap.put(k, v / countMap.get( State.valueOf(k.substring(2)) )) );
 
-        return countMap;
+//        for (State s : State.values()) {
+//            switch (s.name().charAt(0)) {
+//                case 'A':
+//                    countMap.put(s, countMap.get(s)/a);
+//                    break;
+//                case 'B':
+//                    countMap.put(s, countMap.get(s)/b);
+//                    break;
+//                case 'C':
+//                    countMap.put(s, countMap.get(s)/c);
+//                    break;
+//            }
+//        }
+
+        return pairMap;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        double threshold = 90;
+        double threshold = 10;
 
         List<List<Vector>> vecs_alone = ReadData.getVectorsFromFile(new File("train_alone.txt"));
         List<List<Vector>> vecs_group = ReadData.getVectorsFromFile(new File("train_group.txt"));
@@ -100,10 +118,12 @@ public class Classify {
         classifyDifferenceVectors(diff_vecs_alone, threshold);
         classifyDifferenceVectors(diff_vecs_group, threshold);
 
-        Map<State, Double> counted_alone = countClassifications(diff_vecs_alone);
-        Map<State, Double> counted_group = countClassifications(diff_vecs_group);
+        Map<String, Double> counted_alone = countClassifications(diff_vecs_alone);
+        Map<String, Double> counted_group = countClassifications(diff_vecs_group);
 
-        List<List<Vector>> eval_vecs = ReadData.getVectorsFromFile(new File("eval_alone.txt"));
+        System.out.println(counted_alone);
+
+        List<List<Vector>> eval_vecs = ReadData.getVectorsFromFile(new File("eval_group.txt"));
 
         int count_alone = 0;
         int count_group = 0;
@@ -120,13 +140,16 @@ public class Classify {
             double tmp_alone = 0;
             double tmp_group = 0;
 
-            //Probably the mistake : -Math.log is greater, the lesser the ingoing number is
-            for (DifferenceVector eval_vec : eval_diff_vecs) {
-                tmp_alone += -Math.log(counted_alone.get(eval_vec.getState()));
-                tmp_group += -Math.log(counted_group.get(eval_vec.getState()));
+//            for (DifferenceVector eval_vec : eval_diff_vecs) {
+            for (int i = 0; i < eval_diff_vecs.size()-1; i++) {
+                DifferenceVector eval_vec = eval_diff_vecs.get(i);
+                DifferenceVector eval_vec2 = eval_diff_vecs.get(i+1);
+                String state = eval_vec.getState().name() + eval_vec2.getState().name();
+                tmp_alone += -Math.log(counted_alone.get(state));
+                tmp_group += -Math.log(counted_group.get(state));
             }
 
-            if (tmp_alone <= tmp_group) {
+            if (tmp_alone >= tmp_group) {
                 count_group++;
             } else {
                 count_alone++;
